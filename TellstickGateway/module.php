b054c3bfda93c0cf7f44ef43900e6d02f5c6763b
stigkristiansen <stigkristiansen@users.noreply.github.com>
2016-03-28 23:18:07 +0200

    Create module.php

---
+++ TellstickGateway/module.php
@@ -0,0 +1,22 @@
+class TellstickGateway extends IPSModule
+{
+
+    
+    public function Create()
+    {
+        parent::Create();
+        $this->RequireParent("{6DC3D946-0D31-450F-A8C6-C42DB8D7D4F1}");
+
+    }
+
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+        $this->RequestAVRState();
+    }
+	
+	public function ReceiveData($JSONString)
+    {
+	}
+
+}
