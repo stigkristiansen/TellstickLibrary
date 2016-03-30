c3b4e26e47a6b2b11708e71030575910d87e66bf
stigkristiansen <stigkristiansen@users.noreply.github.com>
2016-03-30 14:47:36 +0200

    Update module.php

--- TellstickGateway/module.php
+++ TellstickGateway/module.php
@@ -16,9 +16,18 @@ class TellstickGateway extends IPSModule
         
     }
 	
-    public function ReceiveData($JSONString)
-    {
+    public function ReceiveData($JSONString) {
+ 
+        // Empfangene Daten vom I/O
+        $data = json_decode($JSONString);
+        IPS_LogMessage("ReceiveData", utf8_decode($data->Buffer));
+ 
+        // Hier werden die Daten verarbeitet
+ 
+        // Weiterleitung zu allen Gerät-/Device-Instanzen
+        //$this->SendDataToChildren(json_encode(Array("DataID" => "{66164EB8-3439-4599-B937-A365D7A68567}", "Buffer" => $data->Buffer)));
     }
+
 
 }
 
