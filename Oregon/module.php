e0a45786c24efbedac1cfb49681a9f15b7d21c97
stigkristiansen <stigkristiansen@outlook.com>
2016-03-31 20:40:37 +0200

    .

--- Oregon/module.php
+++ Oregon/module.php
@@ -11,7 +11,7 @@ class OregonWeatherStation extends IPSModule
         parent::Create();
         $this->ConnectParent("{655884D6-7969-4DAF-8992-637BEE9FD70D}");
 		
-		$this->RegisterPropertyInteger ("type", 0 );
+		$this->RegisterPropertyInteger ("model", 0 );
 		$this->RegisterPropertyInteger ("id", 0 );
 
     }
@@ -47,6 +47,8 @@ class OregonWeatherStation extends IPSModule
 	}
 	
 	if(strlen($decodedMessage)>0) {
+		
+		
 		$temperature = GetParameter("temp", $decodedMessage);
 		$humidity = GetParameter("humidity", $decodedMessage);
 	
