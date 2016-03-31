6d929c70aff8741af6f67b33bc9faa08b2d12fe7
stigkristiansen <stigkristiansen@users.noreply.github.com>
2016-03-31 14:34:33 +0200

    Update module.php

--- Oregon/module.php
+++ Oregon/module.php
@@ -32,7 +32,7 @@ class OregonWeatherStation extends IPSModule
 
         $protocol = GetParameter("protocol", $message);
 
-	if(stripos($protocol, "oregon")==false) {
+	if(stripos($protocol, "oregon")!==true) {
 		IPS_LogMessage("OregonSensor", "This is not for me! (unsupportet protocol)");
 		return;
 	}
