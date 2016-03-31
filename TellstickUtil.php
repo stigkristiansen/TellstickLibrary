82773740ebe975d1398396f35ff6bef0f5653cf9
stigkristiansen <stigkristiansen@users.noreply.github.com>
2016-03-31 11:01:35 +0200

    Update TellstickUtil.php

--- TellstickUtil.php
+++ TellstickUtil.php
@@ -17,3 +17,62 @@ function GetParameter($Parameter, $Message) {
 	}
 
 }
+
+function DecodeNexa($Message) {
+
+	$model = GetParameter("model", $Message);
+  	$data = GetParameter("data", $Message) + 0;
+
+	if(stripos($model, "selflearning")!==false)
+	   return DecodeNexaSelflearning($data);
+	else
+	   return DecodeNexaCodeSwitch($data);
+
+}
+
+
+function DecodeNexaSelflearning($Data) {
+//  	IPS_LogMessage("Tellstick 2","Decoding Nexa Selflearning");
+
+  	//$Data = GetParameter("data", $Message) + 0;
+
+  	$house = $Data & 0xFFFFFFC0;
+	$house >>= 6;
+
+	$group = $Data & 0x20;
+	$group >>= 5;
+
+	$method = $Data & 0x10;
+	$method >>= 4;
+
+	$unit = $Data & 0xF;
+	$unit++;
+
+	$stringMethod ="";
+	if($method==1)
+	   $stringMethod="turnon";
+	else if ($method==0)
+	   $stringMethod="turnoff";
+
+
+   $decoded = "class:command;protocol:arctech;model:selflearning;house:".$house.";unit:".$unit.";group:".$group.";method:".$stringMethod;
+
+//  	IPS_LogMessage("Tellstick 2", "Decoded Nexa message: ".$decoded);
+
+  	return $decoded;
+}
+
+function DecodeNexaCodeSwitch($Message) {
+//	IPS_LogMessage("Tellstick 2","Decoding Nexa Code Switch");
+}
+
+function DecodeOregon($Message) {
+
+	$model = GetParameter("model", $Message);
+	$data = GetParameter("data", $Message);
+
+	if(stripos($model, "0xF824")!==false)
+		return DecodeOregonF824($data);
+
+}
+
