3704757f7a0e8f17521863852d22bc6a95ed18ad
stigkristiansen <stigkristiansen@outlook.com>
2016-04-01 23:15:57 +0200

    .

--- Nexa/module.php
+++ Nexa/module.php
@@ -1,33 +1,44 @@
-﻿<?
+<?
 
 require_once(__DIR__ . "/../TellstickUtil.php");  
 
-class NexaSensor extends IPSModule {
+class NexaSensor extends IPSModule
+{
 
     
-    public function Create() {
+    public function Create()
+    {
         parent::Create();
-        
-		$this->ConnectParent("{655884D6-7969-4DAF-8992-637BEE9FD70D}");
+        $this->ConnectParent("{655884D6-7969-4DAF-8992-637BEE9FD70D}");
 		
 		$this->RegisterPropertyInteger ("house", 0 );
 		$this->RegisterPropertyInteger ("unit", 0 );
 
     }
 
-    public function ApplyChanges() {
+    public function ApplyChanges()
+    {
         parent::ApplyChanges();
-		
-		$this->RegisterVariableBoolean( "Status", "Status", "", false);
+        
+        $this->RegisterVariableBoolean( "Status", "Status", "", false );
+
     }
 	
     public function ReceiveData($JSONString) {
-    	$data = json_decode($JSONString);
+    	
+	$data = json_decode($JSONString);
         $message = utf8_decode($data->Buffer);
         
         IPS_LogMessage("Nexa Sensor", "Received ".$message);
+        
+        if($data->DataID!="{F746048C-AAB6-479D-AC48-B4C08875E5CF}") {
+        	IPS_LogMessage("Nexa Sensor", "This is not for me! (unsupported GUID in DataID)");
+        	return;
+        }
 
-	}
+ 
+    }
+
 }
 
 ?>
