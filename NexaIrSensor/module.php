dc988c7311d2be221827517f872e3029d1c9c6eb
stigkristiansen <stigkristiansen@users.noreply.github.com>
2016-04-01 12:56:08 +0200

    Update module.php

--- NexaIrSensor/module.php
+++ NexaIrSensor/module.php
@@ -24,6 +24,8 @@ class NexaIrSensor extends IPSModule
     }
 	
     public function ReceiveData($JSONString) {
+    	
+    	return;
          
         $data = json_decode($JSONString);
         $message = utf8_decode($data->Buffer);
