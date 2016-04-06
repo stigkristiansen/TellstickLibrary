195ea9cde1c2e7b9c7f596e68ee5c5e88b6ad3d4
stigkristiansen <stigkristiansen@users.noreply.github.com>
2016-04-06 13:01:32 +0200

    Update logging.php

--- logging.php
+++ logging.php
@@ -3,15 +3,14 @@ class Logging {
   private $logEnabled;
   private $sender;
   
-  function __construct() 
-    { 
-      $logEnabled = false;  
-    } 
+  function __construct() { 
+    this->logEnabled = false;
+    this->sender = "":
+  } 
     
-    function __construct1($EnableLogging) 
-    { 
-      this->logEnabled = $EnableLogging;
-    } 
+  function __construct1($EnableLogging) { 
+    this->logEnabled = $EnableLogging;
+  } 
     
   public function EnableLogging() {
     this->logEnabled = true;
@@ -24,6 +23,10 @@ class Logging {
   public function LogMessage($Sender, $Message) {
     if($logEnabled)
       IPS_LogMessage($Sender, $Message);
+  }
+  
+  public functio LogMessage($Message) {
+    IPS_LogMessage(this->Sender, $Message);
   }
 
 }
