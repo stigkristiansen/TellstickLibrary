0a62e0ef04beb30783780f59c9522397d07d6358
stigkristiansen <stigkristiansen@users.noreply.github.com>
2016-04-07 12:27:01 +0200

    Update module.php

--- TellstickGateway/module.php
+++ TellstickGateway/module.php
@@ -27,57 +27,72 @@ class TellstickGateway extends IPSModule
     }
 	
     public function ReceiveData($JSONString) {
-				
+	$messages = array();
+	
         $incomingData = json_decode($JSONString);
-		$incomingBuffer = utf8_decode($incomingData->Buffer);
+	$incomingBuffer = utf8_decode($incomingData->Buffer);
 		
-		$log = new Logging($this->ReadPropertyBoolean("log"), IPS_Getname($this->InstanceID));
-		$log->LogMessage("Incoming from serial: ".$incomingBuffer);
+	$log = new Logging($this->ReadPropertyBoolean("log"), IPS_Getname($this->InstanceID));
+	$log->LogMessage("Incoming from serial: ".$incomingBuffer);
 		
-		$bufferId = $this->GetIDForIdent("Buffer");
+	$bufferId = $this->GetIDForIdent("Buffer");
 	
         if (!$this->Lock("ReceiveLock")) {
             $log->LogMessage("Buffer is already locked");
             return false;
         } else
-			$log->LogMessage("Buffer is locked");
+	$log->LogMessage("Buffer is locked");
 
-		$data = GetValueString($bufferId);
-		$data = substr($data, strpos($data, "+W"));
+	$data = GetValueString($bufferId);
+	$data = substr($data, strpos($data, "+W"));
         $data .= $incomingBuffer;
 		
-		do {
-			$foundMessage = false;
-			$arr = str_split($data);
-			$max = sizeof($arr);
-			for($i=0;$i<$max-1;$i++) {
-				if(ord($arr[$i])==0x0D && ord($arr[$i+1])==0x0A) {
-					$message = substr($data, 2, $i-1);
-					$log->LogMessage("Found message: ".$message);
+	do {
+		$foundMessage = false;
+		$arr = str_split($data);
+		$max = sizeof($arr);
+		for($i=0;$i<$max-1;$i++) {
+			if(ord($arr[$i])==0x0D && ord($arr[$i+1])==0x0A) {
+				$message = substr($data, 2, $i-1);
+				$log->LogMessage("Found message: ".$message);
+				if(CheckMessage($messages, $message)) {
 					$this->SendDataToChildren(json_encode(Array("DataID" => "{F746048C-AAB6-479D-AC48-B4C08875E5CF}", "Buffer" => $message)));
 					SetValueString($this->GetIDForIdent("LastCommand"), $message);	
-					
-					$foundMessage = true;
-										
-					if($i!=$max-2)
-						$data = substr($data, $i+2);
-					else
-						$data = "";
-					   
-					break;
+				
+					$messages[]=$message;
 				}
+				$foundMessage = true;
+									
+				if($i!=$max-2)
+					$data = substr($data, $i+2);
+				else
+					$data = "";
+				   
+				break;
 			}
-		} while ($foundMessage && strlen($data)>0);
-		
-		SetValueString($bufferId, $data);
+		}
+	} while ($foundMessage && strlen($data)>0);
+	
+	SetValueString($bufferId, $data);
 
-		$this->Unlock("ReceiveLock");
-        
-		return true;
+	$this->Unlock("ReceiveLock");
+      
+	return true;
     }
-
-	private function Lock($ident)
-    {
+    
+    private function CheckMessage($messages, $message) {
+    	$iMax = len($messages)
+    	for($i=0;$i$iMax) {
+    		if($message==$messages[$i])
+    			return false;
+    	}
+    	
+    	return true;
+    }
+    	
+    	
+   
+    private function Lock($ident)   {
         for ($i = 0; $i < 100; $i++)
         {
             if (IPS_SemaphoreEnter("TSG_" . (string) $this->InstanceID . (string) $ident, 1))
