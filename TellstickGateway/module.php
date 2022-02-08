8291cd517e48f3cfdfa39c515d6633a8072d45ae
stigkristiansen <stigkristiansen@users.noreply.github.com>
2022-02-08 22:03:59 +0100

    Update module.php

--- TellstickGateway/module.php
+++ TellstickGateway/module.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 require_once(__DIR__ . "/../libs/Logging.php");
 require_once(__DIR__ . "/../libs/TellstickUtil.php");
@@ -155,4 +155,4 @@ class TellstickGateway extends IPSModule
     }
 }
 
-?>
+
